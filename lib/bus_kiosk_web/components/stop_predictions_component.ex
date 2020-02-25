38fa3215ee278cd082ca9f9329daae3e40d6f636
Mitchell Henke <mitchell@mitchellhenke.com>
2020-02-25 09:23:49 -0600

    component-ize

---
+++ lib/bus_kiosk_web/components/stop_predictions_component.ex
@@ -0,0 +1,27 @@
+defmodule BusKioskWeb.StopPredictionsComponent do
+  use Phoenix.LiveComponent
+
+  def render(assigns) do
+    ~L"""
+      <div class="prediction-item">
+        <h2 class="route"><%= @stop_name %></h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Route</th>
+              <th>Direction</th>
+              <th>Arrival</th>
+              <th>Est. Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <%= for {route, direction, arrival, predicted_time, trip_id} <- @predictions do  %>
+              <%= live_component(@socket, BusKioskWeb.StopPredictionComponent, id: trip_id, route: route,
+              direction: direction, arrival: arrival, predicted_time: predicted_time) %>
+            <% end %>
+          </tbody>
+        </table>
+      </div>
+    """
+  end
+end
