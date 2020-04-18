acbfab963ecabe649ee79cd671f68a0efd35fdca
Mitchell Henke <mitchell@mitchellhenke.com>
2020-04-18 10:19:55 -0500

    android support hopefully

--- assets/js/controllers/nearby_stops_controller.js
+++ assets/js/controllers/nearby_stops_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "stimulus"
 
 export default class NearbyStopsController extends Controller {
   connect() {
+    if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+      this.element.classList.remove('hide');
+    }
   }
 
   locate() {
@@ -24,9 +27,51 @@ export default class NearbyStopsController extends Controller {
         })
         .catch(console.error);
     } else {
-      // handle regular non iOS 13+ devices
-    }
+      window.addEventListener('deviceorientation', function(e) {
+        const alpha = e.alpha;
+        const gamma = e.gamma;
+        const beta = e.beta;
 
+        // Convert degrees to radians
+        const alphaRad = alpha * (Math.PI / 180);
+        const betaRad = beta * (Math.PI / 180);
+        const gammaRad = gamma * (Math.PI / 180);
+
+        // Calculate equation components
+        const cA = Math.cos(alphaRad);
+        const sA = Math.sin(alphaRad);
+        const cB = Math.cos(betaRad);
+        const sB = Math.sin(betaRad);
+        const cG = Math.cos(gammaRad);
+        const sG = Math.sin(gammaRad);
+
+        // Calculate A, B, C rotation components
+        const rA = - cA * sG - sA * sB * cG;
+        const rB = - sA * sG + cA * sB * cG;
+        const rC = - cB * cG;
+
+        // Calculate compass heading
+        let compassHeading = Math.atan(rA / rB);
+
+        // Convert from half unit circle to whole unit circle
+        if(rB < 0) {
+          compassHeading += Math.PI;
+        }else if(rA < 0) {
+          compassHeading += 2 * Math.PI;
+        }
+
+        // Convert radians to degrees
+        newHeading *= 180 / Math.PI;
+
+        const heading = data.get("heading")
+
+        if(isValid && (heading === null || (Math.abs(parseFloat(heading) - newHeading) > 10))) {
+          const heading = data.set("heading", newHeading);
+          const loc = { 'heading': newHeading };
+          window.hook.pushEvent('location', loc);
+        }
+      }, false);
+    }
   }
 
   static hooks() {
@@ -46,7 +91,7 @@ export default class NearbyStopsController extends Controller {
           }, function(error) {
             console.error(error);
             hook.pushEvent('location', {'error': error});
-          });
+          }, {enableHighAccuracy: true});
         } else {
           console.error('no location permission');
           hook.pushEvent('location', {'error': 'no_permission'});
