10b8d06795719cf7bf77aea0a098ae287fb8a67e
Mitchell Henke <mitchell@mitchellhenke.com>
2020-12-05 10:15:23 -0600

    update deps

--- config/config.exs
+++ config/config.exs
@@ -41,7 +41,7 @@ config :sentry,
   dsn: System.get_env("SENTRY_DSN"),
   environment_name: :prod,
   enable_source_code_context: true,
-  root_source_code_path: File.cwd!()
+  root_source_code_paths: [File.cwd!()]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
